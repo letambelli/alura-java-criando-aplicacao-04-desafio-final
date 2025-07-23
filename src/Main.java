250d34a2f8de6bb94f64a773d8f9fcb5d9b7b686
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-23 14:23:54 -0300

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        int opcao = 0;
+
         String nome = "Natalie Rushman";
         String tipoConta = "Corrente";
         double saldo = 1600;
@@ -9,5 +14,19 @@ public class Main {
         System.out.println("Tipo de Conta: " + tipoConta);
         System.out.println("Saldo: " + saldo);
         System.out.println("*".repeat(40));
+
+        String menu = """
+                1- Consultar Saldo
+                2- Transferir Valor
+                3- Receber Valor
+                4- Sair
+                
+                Digite sua opção:\s""";
+
+        while (opcao != 4) {
+            System.out.print(menu);
+            opcao = leitura.nextInt();
+        }
+
     }
 }
