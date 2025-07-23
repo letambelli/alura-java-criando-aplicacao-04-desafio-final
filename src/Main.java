1c82de73e61202f5c8b03dac1f96aad54e0427f8
Leticia Tambelli <leticiatambelli2@gmail.com>
2025-07-23 12:50:06 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Natalie Rushman";
+        String tipoConta = "Corrente";
+        double saldo = 1600;
+
+        System.out.println("*".repeat(40));
+        System.out.println("Nome do Cliente: " + nome);
+        System.out.println("Tipo de Conta: " + tipoConta);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("*".repeat(40));
+    }
+}
